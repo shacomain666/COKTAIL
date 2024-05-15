7e60fe4c467df9c0dac66551c5883e6d20eeabf2
shacomain666 <warwickrocklee666@mail.ru>
2024-05-15 23:55:55 +0300

    add chat

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ window.addEventListener("scroll", () => {
         btnTop.classList.remove("active");
     }
 })
+
+// const supChat = document.querySelector(".support-chat-window")
+
+// document.addEventListener("click", () => {
+//     if(supChat == "none") {
+//         document.querySelector("support-chat-window").
+//     } else {
+//         document.classList.remove("open") 
+//     }
+// })
+
+function open(chat) {
+    display = document.getElementById("box").style.display;
+    if(display == "none") {
+        document.getElementById("box").style.display = "block"
+    } else {
+        document.getElementById("box").style.display = "none"
+    }
+}
